4285a231311dc79557757ca2ad3eb4c49a25d214
brunodemattos <brunodemattos1998@gmail.com>
2022-11-09 12:16:20 -0300

    CollectUnits waas fixed

--- carcara/src/checker/compression/mod.rs
+++ carcara/src/checker/compression/mod.rs
@@ -1,24 +1,22 @@
 use crate::{ast::*};
+use std::collections::HashMap;
 use std::collections::VecDeque;
-use std::collections::HashSet;
 
-fn visit(id : String, idx : usize, visited_nodes: &mut HashSet<String>, unit_nodes : &mut Vec<(String, usize)>, inserted : &mut HashSet<String>) -> (){
-    if visited_nodes.contains(&id) && !inserted.contains(&id) {
-        let cpy = id.clone();
-        unit_nodes.push((id, idx));
-        inserted.insert(cpy);
+fn visit(idx: usize, visited: &mut HashMap<usize, i32>, unit_nodes: &mut Vec<usize>) -> (){
+    if !visited.contains_key(&idx) {
+        visited.insert(idx, 0);
     }
-    else {
-        visited_nodes.insert(id);
+    else if visited[&idx] == 0 {
+        unit_nodes.push(idx);
+        *visited.get_mut(&idx).unwrap() = 1;
     }
 }
 
-fn collect_units(proof : &Proof) -> Vec<(String, usize)> {
+fn collect_units(proof : &Proof) -> Vec<usize> {
     let mut curr = proof.commands.len() - 1;
     let mut queue = VecDeque::new();
-    let mut visited_nodes = HashSet::new();
+    let mut visited = HashMap::new();
     let mut unit_nodes = Vec::new();
-    let mut inserted = HashSet::new();
     queue.push_back(curr);
 
     //bottom up dfs to go through the proof
@@ -35,22 +33,19 @@ fn collect_units(proof : &Proof) -> Vec<(String, usize)> {
 
                 //if it is a unit clause, then visit it
                 if step.clause.len() == 1{
-                    let id = step.id.clone();
-                    visit(id, curr, &mut visited_nodes, &mut unit_nodes, &mut inserted);
+                    visit(curr, &mut visited, &mut unit_nodes);
                 }
             }
             ProofCommand::Assume {id: _, term} => {
                 match &**term {
                     //if it is a terminal, then it is a unit clause
                     Term::Terminal(_) => {
-                        let id = format!("{:?}", term);
-                        visit(id, curr, &mut visited_nodes, &mut unit_nodes, &mut inserted);
+                        visit(curr, &mut visited, &mut unit_nodes);
                     }
                     Term::Op(_operator, terms) => {
                         //only visit it if it is a unit clause
                         if terms.len() == 1{
-                            let id = format!("{:?}", term);
-                            visit(id, curr, &mut visited_nodes, &mut unit_nodes, &mut inserted);
+                            visit(curr, &mut visited, &mut unit_nodes);
                         }
                     }
                     _ => {}
@@ -58,14 +53,23 @@ fn collect_units(proof : &Proof) -> Vec<(String, usize)> {
             }
             _ => {}
         }
-   
     }
 
+    println!("unit_nodes: {:?}", unit_nodes);
+    println!("visited: {:?}", visited);
     return unit_nodes;
 }
 
+fn fix_proof(proof: &Proof, unit_nodes: Vec<usize>){
+    let mut dnm = Vec::new();
+    dnm.resize(proof.commands.len(), 0);
+    for i in unit_nodes{
+        dnm[i] = 1;
+    }
 
+}
 
-pub fn compress_proof(proof : &Proof){
-    let _unit_nodes = collect_units(&proof);
+pub fn compress_proof(proof: &Proof){
+    let unit_nodes = collect_units(&proof);
+    fix_proof(proof, unit_nodes)
 }
