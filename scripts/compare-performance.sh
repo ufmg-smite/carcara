266d1c8432e08344b3552f8c4a013bd78c3e39bb
vinciusb <65973642+vinciusb@users.noreply.github.com>
2023-03-16 07:10:09 -0300

    Incremented stack size for performance comparison

--- scripts/compare-performance.sh
+++ scripts/compare-performance.sh
@@ -9,18 +9,17 @@ CARCARA=$(realpath $1)
 DIR=$(realpath $2)
 cur_dir=$(realpath $(dirname ${BASH_SOURCE[0]}))/..
 FILE=$2
+# Defines stack size (128 * 1024 * 1024) for deep and big proofs
+ulimit -s 134217728
 
 cd $cur_dir
-echo "AAAA"
-for i in seq 1 $3; do
-    RESULT1=$(cargo run --quiet --release -- check --skip-unknown-rules "$FILE.proof" "$FILE" )
-    # 2>/dev/null
+for i in $(seq 1 $3); do
+    RESULT1=$(cargo run --quiet --release -- check --skip-unknown-rules "$FILE.proof" "$FILE" 2>/dev/null)
     echo $RESULT1
 done
 
 cd $cur_dir
-echo "bbbb"
-for i in seq 1 $3; do
-    RESULT2=$(cargo run --quiet --release --features thread-safety -- check --skip-unknown-rules -u 3 "$FILE.proof" "$FILE" 2>/dev/null)
+for i in $(seq 1 $3); do
+    RESULT2=$(cargo run --quiet  --release --features thread-safety -- check --skip-unknown-rules -u 3 "$FILE.proof" "$FILE" 2>/dev/null)
     echo $RESULT2
 done
