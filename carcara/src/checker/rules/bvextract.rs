3c2785c7611c44c0216f62d50301b4da5561e722
GuilhermeLLS <guiluizls@gmail.com>
2023-11-24 14:55:32 -0300

    refactor: bvextract rule

--- carcara/src/checker/rules/bvextract.rs
+++ carcara/src/checker/rules/bvextract.rs
@@ -1,42 +1,38 @@
 use crate::{
-    ast::{IndexedOperator, Operator, Term},
-    checker::{
-        error::{CheckerError, ExtractError},
-        rules::assert_clause_len,
-    },
+    ast::{Constant, IndexedOperator, Operator, Term},
+    checker::rules::assert_clause_len,
 };
 
-use super::{RuleArgs, RuleResult};
+use super::{assert_eq, RuleArgs, RuleResult};
 
-// ((_ extract i j) x) ≃ (bbterm xj;...;xi)
-pub fn extract(RuleArgs { conclusion, .. }: RuleArgs) -> RuleResult {
+pub fn extract(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
     assert_clause_len(conclusion, 1)?;
-    let (left, right) = match_term_err!((= l r) = &conclusion[0])?;
-    let left_terms = match left.as_ref() {
-        Term::IndexedOp {
-            op: IndexedOperator::BvExtract,
-            op_args,
-            args,
-        } => {
-            let i = op_args[0].as_integer().unwrap().to_usize().unwrap();
-            let j = op_args[1].as_integer().unwrap().to_usize().unwrap();
-            let x = args[0].clone();
-            Ok((i, j, x))
+    let (((_, left_j), left_x), right) =
+        match_term_err!((= ((_ extract i j) x) (bbterm ...)) = &conclusion[0])?;
+
+    let left_j = left_j.as_integer().unwrap();
+    let mut index = left_j;
+
+    if let Some((Operator::BvBbTerm, args)) = left_x.as_op() {
+        let mut index = index.to_usize().unwrap();
+        for arg in right {
+            assert_eq(&args[index], arg)?;
+            index += 1;
         }
-        _ => Err(ExtractError::WrongLeftTerm),
-    };
-    let (left_i, left_j, left_x) = left_terms.unwrap();
-    let size = left_i - left_j + 1;
-    match right.as_ref() {
-        Term::Op(Operator::BvBbTerm, args) => {
-            assert_eq!(args.len(), size);
-            let (_, left_x_args) = left_x.as_op().unwrap();
-            // dbg!(args, left_x_args);
-            assert_eq!(args, &left_x_args[left_j..=left_i]);
-            Ok(())
-        }
-        _ => Err(CheckerError::Extract(ExtractError::WrongRightTerm)),
+        return Ok(());
     }
+
+    for arg in right {
+        let expected_arg = Term::IndexedOp {
+            op: IndexedOperator::BvBitOf,
+            op_args: vec![Constant::Integer(index.clone())],
+            args: vec![left_x.clone()],
+        };
+        let new_arg = pool.add(expected_arg);
+        assert_eq(&new_arg, arg)?;
+        index += 1;
+    }
+    Ok(())
 }
 
 #[cfg(test)]
@@ -47,11 +43,13 @@ mod tests {
         test_cases! {
             definitions = "
             (declare-fun a () Bool)
+            (declare-fun zz () (_ BitVec 12))
             ",
             "Simple working examples" {
-              // "(step t1 (cl (= ((_ extract 5 3) #b01100000) ((_ extract 4 3) #b01100000))) :rule bitblast_extract)": false,
               "(step t2 (cl (= ((_ extract 1 0) (bbterm ((_ bit_of 0) (ite a #b110 #b011)) ((_ bit_of 1) (ite a #b110 #b011)) ((_ bit_of 2) (ite a #b110 #b011)))) (bbterm ((_ bit_of 0) (ite a #b110 #b011)) ((_ bit_of 1) (ite a #b110 #b011))))) :rule bitblast_extract)": true,
-              // "(step t3 (cl (= ((_ extract 1 0) (bbterm ((_ bit_of 0) (ite a #b110 #b011)) ((_ bit_of 1) (ite a #b110 #b011)) ((_ bit_of 2) (ite a #b110 #b011)))) (bbterm ((_ bit_of 0) (ite a #b110 #b011))))) :rule bitblast_extract)": false,
+              "(step t3 (cl (= ((_ extract 1 0) (bbterm ((_ bit_of 0) (ite a #b110 #b011)) ((_ bit_of 1) (ite a #b110 #b011)) ((_ bit_of 2) (ite a #b110 #b011)))) (bbterm ((_ bit_of 0) (ite a #b111 #b011)) ((_ bit_of 1) (ite a #b111 #b011))))) :rule bitblast_extract)": false,
+              "(step t4 (cl (= ((_ extract 11 4) (bbterm ((_ bit_of 0) zz) ((_ bit_of 1) zz) ((_ bit_of 2) zz) ((_ bit_of 3) zz) ((_ bit_of 4) zz) ((_ bit_of 5) zz) ((_ bit_of 6) zz) ((_ bit_of 7) zz) ((_ bit_of 8) zz) ((_ bit_of 9) zz) ((_ bit_of 10) zz) ((_ bit_of 11) zz))) (bbterm ((_ bit_of 4) zz) ((_ bit_of 5) zz) ((_ bit_of 6) zz) ((_ bit_of 7) zz) ((_ bit_of 8) zz) ((_ bit_of 9) zz) ((_ bit_of 10) zz) ((_ bit_of 11) zz)))) :rule bitblast_extract)": true,
+              "(step t5 (cl (= ((_ extract 11 4) (bbterm ((_ bit_of 0) zz) ((_ bit_of 1) zz) ((_ bit_of 2) zz) ((_ bit_of 3) zz) ((_ bit_of 4) zz) ((_ bit_of 5) zz) ((_ bit_of 6) zz) ((_ bit_of 7) zz) ((_ bit_of 8) zz) ((_ bit_of 9) zz) ((_ bit_of 10) zz) ((_ bit_of 11) zz))) (bbterm ((_ bit_of 3) zz) ((_ bit_of 5) zz) ((_ bit_of 6) zz) ((_ bit_of 7) zz) ((_ bit_of 8) zz) ((_ bit_of 9) zz) ((_ bit_of 10) zz) ((_ bit_of 11) zz)))) :rule bitblast_extract)": false,
             }
         }
     }
