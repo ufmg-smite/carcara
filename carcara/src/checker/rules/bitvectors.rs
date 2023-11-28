381bc6496e93fc9508e03c8c548d0ed29dc0b3aa
Bruno Andreotti <bandreotti@proton.me>
2023-11-28 20:19:22 -0300

    Fix formatting

--- carcara/src/checker/rules/bitvectors.rs
+++ carcara/src/checker/rules/bitvectors.rs
@@ -52,7 +52,7 @@ pub fn ult(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
 pub fn add(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
     assert_clause_len(conclusion, 1)?;
     let ((x, y), res) = match_term_err!((= (bvadd x y) res) = &conclusion[0])?;
-  
+
     let Sort::BitVec(size) = pool.sort(x).as_sort().cloned().unwrap() else {
         unreachable!();
     };
@@ -61,7 +61,7 @@ pub fn add(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
 
     let x = build_term_vec(x, size, pool);
     let y = build_term_vec(y, size, pool);
-    
+
     let mut carries = vec![pool.bool_false()];
 
     for i in 1..size {
@@ -91,9 +91,9 @@ mod tests {
     fn ult() {
         test_cases! {
             definitions = "
-    (declare-fun x4 () (_ BitVec 4))
-    (declare-fun y4 () (_ BitVec 4))
-    ",
+                (declare-fun x4 () (_ BitVec 4))
+                (declare-fun y4 () (_ BitVec 4))
+            ",
             "Using bvult with x and y as bitvectors" {
               "(step t3 (cl (= (bvult x4 y4) (or (= ((_ bit_of 3) x4) ((_ bit_of 2) y4)) ((_ bit_of 3) x4) ((_ bit_of 2) y4)))) :rule bitblast_ult)": false,
               "(step t3 (cl (= (bvult x4 y4) (or (and (= ((_ bit_of 3) x4) ((_ bit_of 3) y4)) (or (and (= ((_ bit_of 2) x4) ((_ bit_of 2) y4)) (or (and (= ((_ bit_of 1) x4) ((_ bit_of 1) y4)) (and (not ((_ bit_of 0) x4)) ((_ bit_of 0) y4))) (and (not ((_ bit_of 1) x4)) ((_ bit_of 1) y4)))) (and (not ((_ bit_of 2) x4)) ((_ bit_of 2) y4)))) (and (not ((_ bit_of 3) x4)) ((_ bit_of 3) y4))))) :rule bitblast_ult)": true,
@@ -109,9 +109,9 @@ mod tests {
     fn add() {
         test_cases! {
             definitions = "
-      (declare-fun x4 () (_ BitVec 4))
-      (declare-fun y4 () (_ BitVec 4))
-      ",
+                (declare-fun x4 () (_ BitVec 4))
+                (declare-fun y4 () (_ BitVec 4))
+            ",
             "Using bvadd with x and y as bitvectors" {
               "(step t3 (cl (= (bvadd x4 y4) (bbterm ((_ bit_of 0) x4) ((_ bit_of 1) y4) ((_ bit_of 2) x4) ((_ bit_of 3) y4)))) :rule bitblast_bvadd)": false,
               "(step t4 (cl (= (bvadd x4 y4) (bbterm (xor (xor ((_ bit_of 0) x4) ((_ bit_of 0) y4)) false) (xor (xor ((_ bit_of 1) x4) ((_ bit_of 1) y4)) (or (and ((_ bit_of 0) x4) ((_ bit_of 0) y4)) (and (xor ((_ bit_of 0) x4) ((_ bit_of 0) y4)) false))) (xor (xor ((_ bit_of 2) x4) ((_ bit_of 2) y4)) (or (and ((_ bit_of 1) x4) ((_ bit_of 1) y4)) (and (xor ((_ bit_of 1) x4) ((_ bit_of 1) y4)) (or (and ((_ bit_of 0) x4) ((_ bit_of 0) y4)) (and (xor ((_ bit_of 0) x4) ((_ bit_of 0) y4)) false))))) (xor (xor ((_ bit_of 3) x4) ((_ bit_of 3) y4)) (or (and ((_ bit_of 2) x4) ((_ bit_of 2) y4)) (and (xor ((_ bit_of 2) x4) ((_ bit_of 2) y4)) (or (and ((_ bit_of 1) x4) ((_ bit_of 1) y4)) (and (xor ((_ bit_of 1) x4) ((_ bit_of 1) y4)) (or (and ((_ bit_of 0) x4) ((_ bit_of 0) y4)) (and (xor ((_ bit_of 0) x4) ((_ bit_of 0) y4)) false)))))))))) :rule bitblast_bvadd)": true,
