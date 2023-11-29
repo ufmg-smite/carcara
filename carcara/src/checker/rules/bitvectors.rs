2c2647ceef4608613dcccfbbf375a2af61092104
GuilhermeLLS <guiluizls@gmail.com>
2023-11-29 06:49:04 -0300

    fix: formatting

--- carcara/src/checker/rules/bitvectors.rs
+++ carcara/src/checker/rules/bitvectors.rs
@@ -66,18 +66,18 @@ pub fn add(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
 
     for i in 1..size {
         let carry_i = build_term!(
-        pool,
-        (or (and {x[i - 1].clone()} {y[i - 1].clone()}) (and (xor {x[i - 1].clone()} {y[i - 1].clone()}) {carries[i - 1].clone()}))
-    );
+          pool,
+          (or (and {x[i - 1].clone()} {y[i - 1].clone()}) (and (xor {x[i - 1].clone()} {y[i - 1].clone()}) {carries[i - 1].clone()}))
+        );
         carries.push(carry_i);
     }
 
     let res_args: Vec<_> = (0..size)
         .map(|i| {
             build_term!(
-        pool,
-        (xor (xor {x[i].clone()} {y[i].clone()}) {carries[i].clone()})
-    )
+          pool,
+          (xor (xor {x[i].clone()} {y[i].clone()}) {carries[i].clone()})
+        )
         })
         .collect();
 
@@ -121,9 +121,9 @@ mod tests {
     fn ult() {
         test_cases! {
             definitions = "
-              (declare-fun x4 () (_ BitVec 4))
-              (declare-fun y4 () (_ BitVec 4))
-          ",
+                (declare-fun x4 () (_ BitVec 4))
+                (declare-fun y4 () (_ BitVec 4))
+            ",
             "Using bvult with x and y as bitvectors" {
               "(step t3 (cl (= (bvult x4 y4) (or (= ((_ bit_of 3) x4) ((_ bit_of 2) y4)) ((_ bit_of 3) x4) ((_ bit_of 2) y4)))) :rule bitblast_ult)": false,
               "(step t3 (cl (= (bvult x4 y4) (or (and (= ((_ bit_of 3) x4) ((_ bit_of 3) y4)) (or (and (= ((_ bit_of 2) x4) ((_ bit_of 2) y4)) (or (and (= ((_ bit_of 1) x4) ((_ bit_of 1) y4)) (and (not ((_ bit_of 0) x4)) ((_ bit_of 0) y4))) (and (not ((_ bit_of 1) x4)) ((_ bit_of 1) y4)))) (and (not ((_ bit_of 2) x4)) ((_ bit_of 2) y4)))) (and (not ((_ bit_of 3) x4)) ((_ bit_of 3) y4))))) :rule bitblast_ult)": true,
@@ -139,9 +139,9 @@ mod tests {
     fn add() {
         test_cases! {
             definitions = "
-              (declare-fun x4 () (_ BitVec 4))
-              (declare-fun y4 () (_ BitVec 4))
-          ",
+                (declare-fun x4 () (_ BitVec 4))
+                (declare-fun y4 () (_ BitVec 4))
+            ",
             "Using bvadd with x and y as bitvectors" {
               "(step t3 (cl (= (bvadd x4 y4) (bbterm ((_ bit_of 0) x4) ((_ bit_of 1) y4) ((_ bit_of 2) x4) ((_ bit_of 3) y4)))) :rule bitblast_bvadd)": false,
               "(step t4 (cl (= (bvadd x4 y4) (bbterm (xor (xor ((_ bit_of 0) x4) ((_ bit_of 0) y4)) false) (xor (xor ((_ bit_of 1) x4) ((_ bit_of 1) y4)) (or (and ((_ bit_of 0) x4) ((_ bit_of 0) y4)) (and (xor ((_ bit_of 0) x4) ((_ bit_of 0) y4)) false))) (xor (xor ((_ bit_of 2) x4) ((_ bit_of 2) y4)) (or (and ((_ bit_of 1) x4) ((_ bit_of 1) y4)) (and (xor ((_ bit_of 1) x4) ((_ bit_of 1) y4)) (or (and ((_ bit_of 0) x4) ((_ bit_of 0) y4)) (and (xor ((_ bit_of 0) x4) ((_ bit_of 0) y4)) false))))) (xor (xor ((_ bit_of 3) x4) ((_ bit_of 3) y4)) (or (and ((_ bit_of 2) x4) ((_ bit_of 2) y4)) (and (xor ((_ bit_of 2) x4) ((_ bit_of 2) y4)) (or (and ((_ bit_of 1) x4) ((_ bit_of 1) y4)) (and (xor ((_ bit_of 1) x4) ((_ bit_of 1) y4)) (or (and ((_ bit_of 0) x4) ((_ bit_of 0) y4)) (and (xor ((_ bit_of 0) x4) ((_ bit_of 0) y4)) false)))))))))) :rule bitblast_bvadd)": true,
@@ -156,10 +156,10 @@ mod tests {
     fn extract() {
         test_cases! {
             definitions = "
-              (declare-fun a () Bool)
-              (declare-fun zz () (_ BitVec 12))
-              (declare-fun xx () (_ BitVec 12))
-          ",
+                (declare-fun a () Bool)
+                (declare-fun zz () (_ BitVec 12))
+                (declare-fun xx () (_ BitVec 12))
+            ",
             "Using extract with x and y as bbterms" {
               "(step t2 (cl (= ((_ extract 1 0) (bbterm ((_ bit_of 0) (ite a #b110 #b011)) ((_ bit_of 1) (ite a #b110 #b011)) ((_ bit_of 2) (ite a #b110 #b011)))) (bbterm ((_ bit_of 0) (ite a #b110 #b011)) ((_ bit_of 1) (ite a #b110 #b011))))) :rule bitblast_extract)": true,
               "(step t3 (cl (= ((_ extract 1 0) (bbterm ((_ bit_of 0) (ite a #b110 #b011)) ((_ bit_of 1) (ite a #b110 #b011)) ((_ bit_of 2) (ite a #b110 #b011)))) (bbterm ((_ bit_of 0) (ite a #b111 #b011)) ((_ bit_of 1) (ite a #b111 #b011))))) :rule bitblast_extract)": false,
