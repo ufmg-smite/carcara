ufmg-smite/carcara

commit ce9d5e54099e209f74fca784d31a05b1cc1a619c

Update parser for new syntax of anchors (#37)
* Update parser for new syntax of anchors

* fixing tests

* fix test in documentation (???)

* Update carcara/src/parser/mod.rs

Co-authored-by: Bruno Andreotti <bandreotti@protonmail.com>

---------

Co-authored-by: Bruno Andreotti <bandreotti@protonmail.com>

carcara/src/parser/mod.rs
@@ -1011,14 +1011,13 @@ impl<'a, R: BufRead> Parser<'a, R> {
     }
 
     /// Parses an argument for an `anchor` proof command. This can be either a variable binding of
-    /// the form `(<symbol> <sort>)` or an assignment, of the form `(:= <symbol> <term>)`.
+    /// the form `(<symbol> <sort>)` or an assignment, of the form `(:= (<symbol> <sort>) <term>)`.
     fn parse_anchor_argument(&mut self) -> CarcaraResult<AnchorArg> {
         self.expect_token(Token::OpenParen)?;
         Ok(if self.current_token == Token::Keyword("=".into()) {
             self.next_token()?;
-            let var = self.expect_symbol()?;
-            let value = self.parse_term()?;
-            let sort = self.pool.sort(&value);
+            let (var, sort) = self.parse_sorted_var()?;
+            let value = self.parse_term_expecting_sort(sort.as_sort().unwrap())?;
             self.insert_sorted_var((var.clone(), sort));
             self.expect_token(Token::CloseParen)?;
             AnchorArg::Assign(var, value)

carcara/src/ast/iter.rs
@@ -19,7 +19,7 @@ use super::*;
 /// let proof = "
 ///     (assume h1 (forall ((x Int)) (> x 0)))
 ///     (assume h2 (not (forall ((y Int)) (> y 0))))
-///     (anchor :step t3 :args ((y Int) (:= x y)))
+///     (anchor :step t3 :args ((y Int) (:= (x Int) y)))
 ///     (step t3.t1 (cl (= x y)) :rule refl)
 ///     (step t3.t2 (cl (= (> x 0) (> y 0))) :rule cong :premises (t3.t1))
 ///     (step t3 (cl (= (forall ((x Int)) (> x 0)) (forall ((y Int)) (> y 0)))) :rule bind)

carcara/src/checker/rules/reflexivity.rs
@@ -218,7 +218,7 @@ mod tests {
                 (declare-fun z () Real)
             ",
             "Simple working examples" {
-                "(anchor :step t1 :args ((y Real) (:= x y)))
+                "(anchor :step t1 :args ((y Real) (:= (x Real) y)))
                 (step t1.t1 (cl (= x y)) :rule refl)
                 (step t1 (cl) :rule hole)": true,
 
@@ -228,7 +228,7 @@ mod tests {
 
             }
             "Multiple substitutions in sequence" {
-                "(anchor :step t1 :args ((z Real) (:= y z) (:= x y)))
+                "(anchor :step t1 :args ((z Real) (:= (y Real) z) (:= (x Real) y)))
                 (step t1.t1 (cl (= x z)) :rule refl)
                 (step t1 (cl) :rule hole)": true,
             }
@@ -236,20 +236,20 @@ mod tests {
                 // Since an inner subproof cannot end an outer subproof, we need to have a dummy
                 // step to the end the outer subproofs in these examples
 
-                "(anchor :step t1 :args ((z Real) (:= x z)))
-                (anchor :step t1.t1 :args ((z Real) (:= y z)))
+                "(anchor :step t1 :args ((z Real) (:= (x Real) z)))
+                (anchor :step t1.t1 :args ((z Real) (:= (y Real) z)))
                 (step t1.t1.t1 (cl (= x y)) :rule refl)
                 (step t1.t1 (cl) :rule hole)
                 (step t1 (cl) :rule hole)": true,
 
-                "(anchor :step t1 :args ((y Real) (:= x y)))
-                (anchor :step t1.t1 :args ((z Real) (:= y z)))
+                "(anchor :step t1 :args ((y Real) (:= (x Real) y)))
+                (anchor :step t1.t1 :args ((z Real) (:= (y Real) z)))
                 (step t1.t1.t1 (cl (= x z)) :rule refl)
                 (step t1.t1 (cl) :rule hole)
                 (step t1 (cl) :rule hole)": true,
             }
             "Terms aren't equal after applying context substitution" {
-                "(anchor :step t1 :args ((y Real) (:= x y)))
+                "(anchor :step t1 :args ((y Real) (:= (x Real) y)))
                 (step t1.t1 (cl (= x z)) :rule refl)
                 (step t1 (cl) :rule hole)": false,
             }

carcara/src/checker/rules/subproof.rs
@@ -489,57 +489,57 @@ mod tests {
                 (declare-fun y () Real)
             ",
             "Simple working examples" {
-                "(anchor :step t1 :args ((y Real) (:= x y)))
+                "(anchor :step t1 :args ((y Real) (:= (x Real) y)))
                 (step t1.t1 (cl (= p q)) :rule hole)
                 (step t1 (cl (= (forall ((x Real)) p) (forall ((y Real)) q))) :rule bind)": true,
 
-                "(anchor :step t1 :args ((y1 Real) (y2 Real) (:= x1 y1) (:= x2 y2)))
+                "(anchor :step t1 :args ((y1 Real) (y2 Real) (:= (x1 Real) y1) (:= (x2 Real) y2)))
                 (step t1.t1 (cl (= (= x1 x2) (= y1 y2))) :rule hole)
                 (step t1 (cl (= (forall ((x1 Real) (x2 Real)) (= x1 x2))
                     (forall ((y1 Real) (y2 Real)) (= y1 y2)))) :rule bind)": true,
             }
             "Examples with binding arguments" {
-                "(anchor :step t1 :args ((y Real) (z Real) (:= x y)))
+                "(anchor :step t1 :args ((y Real) (z Real) (:= (x Real) y)))
                 (step t1.t1 (cl (= p q)) :rule hole)
                 (step t1 (cl (= (forall ((x Real) (z Real)) p)
                     (forall ((y Real) (z Real)) q))) :rule bind)": true,
             }
             "Binding `lambda` and `choice` terms" {
-                "(anchor :step t1 :args ((y Real) (:= x y)))
+                "(anchor :step t1 :args ((y Real) (:= (x Real) y)))
                 (step t1.t1 (cl (= x y)) :rule hole)
                 (step t1 (cl (= (lambda ((x Real)) x) (lambda ((y Real)) y))) :rule bind)": true,
 
-                "(anchor :step t1 :args ((y Int) (:= x y)))
+                "(anchor :step t1 :args ((y Int) (:= (x Int) y)))
                 (step t1.t1 (cl (= p q)) :rule hole)
                 (step t1 (cl (= (choice ((x Int)) p) (choice ((y Int)) q))) :rule bind)": true,
             }
             "y_i appears in phi as a free variable" {
-                "(anchor :step t1 :args ((y Real) (:= x y)))
+                "(anchor :step t1 :args ((y Real) (:= (x Real) y)))
                 (step t1.t1 (cl (= (= y x) (= y y))) :rule hole)
                 (step t1 (cl (= (forall ((x Real)) (= y x))
                     (forall ((y Real)) (= y y)))) :rule bind)": false,
 
-                "(anchor :step t1 :args ((y Real) (z Real) (:= x y)))
+                "(anchor :step t1 :args ((y Real) (z Real) (:= (x Real) y)))
                 (step t1.t1 (cl (= (= y x) (= y y))) :rule hole)
                 (step t1 (cl (= (forall ((z Real) (x Real)) (= y z))
                     (forall ((z Real) (y Real)) (= y z)))) :rule bind)": false,
             }
             "Terms in conclusion clause don't match terms in previous command" {
-                "(anchor :step t1 :args ((y Real) (:= x y)))
+                "(anchor :step t1 :args ((y Real) (:= (x Real) y)))
                 (step t1.t1 (cl (= p q)) :rule hole)
                 (step t1.t2 (cl (= r s)) :rule hole) ; This step shouldn't be here!
                 (step t1 (cl (= (forall ((x Real)) p) (forall ((y Real)) q))) :rule bind)": false,
 
-                "(anchor :step t1 :args ((y Real) (:= x y)))
+                "(anchor :step t1 :args ((y Real) (:= (x Real) y)))
                 (step t1.t1 (cl (= p q)) :rule hole)
                 (step t1 (cl (= (forall ((x Real)) q) (forall ((y Real)) p))) :rule bind)": false,
             }
             "Context substitutions don't match quantifier bindings" {
-                "(anchor :step t1 :args ((y Real) (:= x y)))
+                "(anchor :step t1 :args ((y Real) (:= (x Real) y)))
                 (step t1.t1 (cl (= p q)) :rule hole)
                 (step t1 (cl (= (forall ((y Real)) p) (forall ((x Real)) q))) :rule bind)": false,
 
-                "(anchor :step t1 :args ((y1 Real) (y2 Real) (:= x1 y1) (:= x2 y2)))
+                "(anchor :step t1 :args ((y1 Real) (y2 Real) (:= (x1 Real) y1) (:= (x2 Real) y2)))
                 (step t1.t1 (cl (= (= x1 x2) (= y1 y2))) :rule hole)
                 (step t1 (cl (= (forall ((x2 Real)) (= x1 x2))
                     (forall ((y1 Real) (y2 Real)) (= y1 y2)))) :rule bind)": false,
@@ -562,45 +562,45 @@ mod tests {
             ",
             "Simple working examples" {
                 "(step t1 (cl (= i x)) :rule hole)
-                (anchor :step t2 :args ((x Int) (:= a x)))
+                (anchor :step t2 :args ((x Int) (:= (a Int) x)))
                 (step t2.t1 (cl (= p q)) :rule hole)
                 (step t2 (cl (= (let ((a i)) p) q)) :rule let :premises (t1))": true,
 
                 "(step t1 (cl (= i x)) :rule hole)
                 (step t2 (cl (= k z)) :rule hole)
-                (anchor :step t3 :args ((x Int) (y Int) (z Int) (:= a x) (:= b y) (:= c z)))
+                (anchor :step t3 :args ((x Int) (y Int) (z Int) (:= (a Int) x) (:= (b Int) y) (:= (c Int) z)))
                 (step t3.t1 (cl (= p q)) :rule hole)
                 (step t3 (cl (= (let ((a i) (b y) (c k)) p) q)) :rule let :premises (t1 t2))": true,
             }
             "Premise equalities may be flipped" {
                 "(step t1 (cl (= x i)) :rule hole)
-                (anchor :step t2 :args ((x Int) (:= a x)))
+                (anchor :step t2 :args ((x Int) (:= (a Int) x)))
                 (step t2.t1 (cl (= p q)) :rule hole)
                 (step t2 (cl (= (let ((a i)) p) q)) :rule let :premises (t1))": true,
             }
             "Wrong number of premises" {
                 "(step t1 (cl (= i x)) :rule hole)
-                (anchor :step t2 :args ((x Int) (y Int) (z Int) (:= a x) (:= b y) (:= c z)))
+                (anchor :step t2 :args ((x Int) (y Int) (z Int) (:= (a Int) x) (:= (b Int) y) (:= (c Int) z)))
                 (step t2.t1 (cl (= p q)) :rule hole)
                 (step t2 (cl (= (let ((a i) (b y) (c k)) p) q)) :rule let :premises (t1))": false,
 
                 "(step t1 (cl (= i x)) :rule hole)
                 (step t2 (cl (= y y)) :rule hole)
                 (step t3 (cl (= k z)) :rule hole)
-                (anchor :step t4 :args ((x Int) (y Int) (z Int) (:= a x) (:= b y) (:= c z)))
+                (anchor :step t4 :args ((x Int) (y Int) (z Int) (:= (a Int) x) (:= (b Int) y) (:= (c Int) z)))
                 (step t4.t1 (cl (= p q)) :rule hole)
                 (step t4 (cl (= (let ((a i) (b y) (c k)) p) q)) :rule let :premises (t1 t2))": false,
             }
             "Number of bindings is `let` term doesn't match number of substitutions in context" {
                 "(step t1 (cl (= i x)) :rule hole)
                 (step t2 (cl (= j y)) :rule hole)
-                (anchor :step t3 :args ((x Int) (y Int) (z Int) (:= a x) (:= b y) (:= c z)))
+                (anchor :step t3 :args ((x Int) (y Int) (z Int) (:= (a Int) x) (:= (b Int) y) (:= (c Int) z)))
                 (step t3.t1 (cl (= p q)) :rule hole)
                 (step t3 (cl (= (let ((a i) (b j)) p) q)) :rule let :premises (t1 t2))": false,
             }
             "u and u' don't match equality in previous command" {
                 "(step t1 (cl (= i x)) :rule hole)
-                (anchor :step t2 :args ((x Int) (:= a x)))
+                (anchor :step t2 :args ((x Int) (:= (a Int) x)))
                 (step t2.t1 (cl (= p (= i j))) :rule hole)
                 (step t2 (cl (= (let ((a i)) p) q)) :rule let :premises (t1))": false,
             }
@@ -612,23 +612,23 @@ mod tests {
         test_cases! {
             definitions = "(declare-fun p () Bool)",
             "Simple working examples" {
-                "(anchor :step t1 :args ((t Int) (:= x t)))
+                "(anchor :step t1 :args ((t Int) (:= (x Int) t)))
                 (step t1.t1 (cl (= (=> (= x t) p) (=> (= t t) p))) :rule hole)
                 (step t1 (cl (= (forall ((x Int)) (=> (= x t) p)) (=> (= t t) p)))
                     :rule onepoint)": true,
 
-                "(anchor :step t1 :args ((t Int) (:= x t)))
+                "(anchor :step t1 :args ((t Int) (:= (x Int) t)))
                 (step t1.t1 (cl (= (or (not (= x t)) p) (or (not (= t t)) p))) :rule hole)
                 (step t1 (cl (= (forall ((x Int)) (or (not (= x t)) p)) (or (not (= t t)) p)))
                     :rule onepoint)": true,
 
-                "(anchor :step t1 :args ((t Int) (:= x t)))
+                "(anchor :step t1 :args ((t Int) (:= (x Int) t)))
                 (step t1.t1 (cl (= (and (= x t) p) (and (= t t) p))) :rule hole)
                 (step t1 (cl (= (exists ((x Int)) (and (= x t) p)) (and (= t t) p)))
                     :rule onepoint)": true,
             }
             "Multiple quantifier bindings" {
-                "(anchor :step t1 :args ((x Int) (y Int) (t Int) (:= z t)))
+                "(anchor :step t1 :args ((x Int) (y Int) (t Int) (:= (z Int) t)))
                 (step t1.t1 (cl (= (=> (= z t) (= (+ x y) (+ z t)))
                                    (=> (= t t) (= (+ x y) (+ t t))))) :rule hole)
                 (step t1 (cl (=
@@ -636,7 +636,7 @@ mod tests {
                     (forall ((x Int) (y Int))         (=> (= t t) (= (+ x y) (+ t t))))
                 )) :rule onepoint)": true,
 
-                "(anchor :step t1 :args ((x Int) (y Int) (t Int) (:= z t)))
+                "(anchor :step t1 :args ((x Int) (y Int) (t Int) (:= (z Int) t)))
                 (step t1.t1 (cl (= (and (= z t) (= (+ x y) (+ z t)))
                                    (and (= t t) (= (+ x y) (+ t t))))) :rule hole)
                 (step t1 (cl (=
@@ -645,7 +645,7 @@ mod tests {
                 )) :rule onepoint)": true,
             }
             "Multiple quantifier bindings eliminated" {
-                "(anchor :step t1 :args ((t Int) (u Int) (v Int) (:= x t) (:= y u) (:= z v)))
+                "(anchor :step t1 :args ((t Int) (u Int) (v Int) (:= (x Int) t) (:= (y Int) u) (:= (z Int) v)))
                 (step t1.t1 (cl (= (=> (= x t) (=> (= y u) (=> (= z v) p)))
                                    (=> (= t t) (=> (= u u) (=> (= v v) p))))) :rule hole)
                 (step t1 (cl (=
@@ -653,7 +653,7 @@ mod tests {
                     (=> (= t t) (=> (= u u) (=> (= v v) p)))
                 )) :rule onepoint)": true,
 
-                "(anchor :step t1 :args ((t Int) (u Int) (v Int) (:= x t) (:= y u) (:= z v)))
+                "(anchor :step t1 :args ((t Int) (u Int) (v Int) (:= (x Int) t) (:= (y Int) u) (:= (z Int) v)))
                 (step t1.t1 (cl (= (or (not (= x t)) (or (not (= y u)) (or (not (= z v)) p)))
                                    (or (not (= t t)) (or (not (= u u)) (or (not (= v v)) p)))
                 )) :rule hole)
@@ -663,7 +663,7 @@ mod tests {
                     (or (not (= t t)) (or (not (= u u)) (or (not (= v v)) p)))
                 )) :rule onepoint)": true,
 
-                "(anchor :step t1 :args ((t Int) (u Int) (v Int) (:= x t) (:= y u) (:= z v)))
+                "(anchor :step t1 :args ((t Int) (u Int) (v Int) (:= (x Int) t) (:= (y Int) u) (:= (z Int) v)))
                 (step t1.t1 (cl (= (=> (and (= x t) (and (= y u) (= z v))) p)
                                    (=> (and (= t t) (and (= u u) (= v v))) p))) :rule hole)
                 (step t1 (cl (=
@@ -671,7 +671,7 @@ mod tests {
                     (=> (and (= t t) (and (= u u) (= v v))) p)
                 )) :rule onepoint)": true,
 
-                "(anchor :step t1 :args ((t Int) (u Int) (v Int) (:= x t) (:= y u) (:= z v)))
+                "(anchor :step t1 :args ((t Int) (u Int) (v Int) (:= (x Int) t) (:= (y Int) u) (:= (z Int) v)))
                 (step t1.t1 (cl (= (and (= x t) (and (= y u) (and (= z v) p)))
                                    (and (= t t) (and (= u u) (and (= v v) p))))) :rule hole)
                 (step t1 (cl (=
@@ -690,14 +690,14 @@ mod tests {
                 (declare-fun q (Int) Bool)
             ",
             "Simple working examples" {
-                "(anchor :step t1 :args ((:= x (choice ((x Int)) (p x)))))
+                "(anchor :step t1 :args ((:= (x Int) (choice ((x Int)) (p x)))))
                 (step t1.t1 (cl (= (p x) (p (choice ((x Int)) (p x))))) :rule hole)
                 (step t1 (cl (= (exists ((x Int)) (p x)) (p (choice ((x Int)) (p x)))))
                     :rule sko_ex)": true,
 
                 "(anchor :step t1 :args (
-                    (:= x (choice ((x Int)) (exists ((y Int)) (= x y))))
-                    (:= y (choice ((y Int)) (= (choice ((x Int)) (exists ((y Int)) (= x y))) y)))
+                    (:= (x Int) (choice ((x Int)) (exists ((y Int)) (= x y))))
+                    (:= (y Int) (choice ((y Int)) (= (choice ((x Int)) (exists ((y Int)) (= x y))) y)))
                 ))
                 (step t1.t1 (cl (=
                     (= x y)
@@ -721,14 +721,14 @@ mod tests {
                 (declare-fun q (Int) Bool)
             ",
             "Simple working examples" {
-                "(anchor :step t1 :args ((:= x (choice ((x Int)) (not (p x))))))
+                "(anchor :step t1 :args ((:= (x Int) (choice ((x Int)) (not (p x))))))
                 (step t1.t1 (cl (= (p x) (p (choice ((x Int)) (not (p x)))))) :rule hole)
                 (step t1 (cl (= (forall ((x Int)) (p x)) (p (choice ((x Int)) (not (p x))))))
                     :rule sko_forall)": true,
 
                 "(anchor :step t1 :args (
-                    (:= x (choice ((x Int)) (not (forall ((y Int)) (= x y)))))
-                    (:= y
+                    (:= (x Int) (choice ((x Int)) (not (forall ((y Int)) (= x y)))))
+                    (:= (y Int)
                         (choice ((y Int))
                             (not (= (choice ((x Int)) (not (forall ((y Int)) (= x y)))) y))))
                 ))

commit 340c2f35a1dbb414da2378603137286ad1ed9129

Use SortedVar rather than String for vars in anchor assingnments
This is necessary to fix a bug (introduced in
ce9d5e54099e209f74fca784d31a05b1cc1a619c) where elaborated proofs will not be
checkable by Carcara itself (since the printer was not printing anchors
according to the new format).

carcara/src/ast/context.rs
@@ -12,7 +12,7 @@ impl Context {
     /// `cumulative_substitution` field.
     fn build(
         pool: &mut dyn TermPool,
-        assignment_args: &[(String, Rc<Term>)],
+        assignment_args: &[(SortedVar, Rc<Term>)],
         variable_args: &[SortedVar],
     ) -> Result<Self, SubstitutionError> {
         // We build the context mappings incrementally, using the mappings already
@@ -25,7 +25,7 @@ impl Context {
         let mappings = assignment_args
             .iter()
             .map(|(var, value)| {
-                let var_term = pool.add(Term::new_var(var, pool.sort(value)));
+                let var_term = pool.add(Term::Var(var.0.clone(), var.1.clone()));
                 let new_value = substitution.apply(pool, value);
                 substitution.insert(pool, var_term.clone(), new_value.clone())?;
                 Ok((var_term, new_value))
@@ -130,7 +130,7 @@ impl ContextStack {
     pub fn push(
         &mut self,
         pool: &mut dyn TermPool,
-        assignment_args: &[(String, Rc<Term>)],
+        assignment_args: &[(SortedVar, Rc<Term>)],
         variable_args: &[SortedVar],
         context_id: usize,
     ) -> Result<(), SubstitutionError> {

carcara/src/ast/mod.rs
@@ -156,7 +156,7 @@ pub struct Subproof {
     pub commands: Vec<ProofCommand>,
 
     /// The "assignment" style arguments of the subproof, of the form `(:= <symbol> <term>)`.
-    pub assignment_args: Vec<(String, Rc<Term>)>,
+    pub assignment_args: Vec<(SortedVar, Rc<Term>)>,
 
     /// The "variable" style arguments of the subproof, of the form `(<symbol> <sort>)`.
     pub variable_args: Vec<SortedVar>,

carcara/src/ast/printer.rs
@@ -153,12 +153,14 @@ impl<'a> PrintProof for AlethePrinter<'a> {
                             is_first = false;
                             var.print_with_sharing(self)?;
                         }
-                        for (name, value) in &s.assignment_args {
+                        for (var, value) in &s.assignment_args {
                             if !is_first {
                                 write!(self.inner, " ")?;
                             }
                             is_first = false;
-                            write!(self.inner, "(:= {} ", name)?;
+                            write!(self.inner, "(:= ")?;
+                            var.print_with_sharing(self)?;
+                            write!(self.inner, " ")?;
                             value.print_with_sharing(self)?;
                             write!(self.inner, ")")?;
                         }

carcara/src/elaborator/accumulator.rs
@@ -50,7 +50,7 @@ impl Accumulator {
 
     pub fn close_subproof(
         &mut self,
-        assignment_args: Vec<(String, Rc<Term>)>,
+        assignment_args: Vec<(SortedVar, Rc<Term>)>,
         variable_args: Vec<SortedVar>,
         root_id: &str,
     ) -> ProofCommand {

carcara/src/elaborator/mod.rs
@@ -150,7 +150,7 @@ impl Elaborator {
     /// make sure it is the next `id` in the outer subproof.
     pub fn close_accumulator_subproof(
         &mut self,
-        assignment_args: Vec<(String, Rc<Term>)>,
+        assignment_args: Vec<(SortedVar, Rc<Term>)>,
         variable_args: Vec<SortedVar>,
         end_step: ProofStep,
         root_id: &str,

carcara/src/elaborator/polyeq.rs
@@ -94,7 +94,7 @@ impl<'a> PolyeqElaborator<'a> {
                         let assignment_args: Vec<_> = a_bindings
                             .iter()
                             .map(|x| {
-                                let var = x.0.clone();
+                                let var = (x.0.clone(), pool.sort(&x.1));
                                 let term = pool.add(x.clone().into());
                                 (var, term)
                             })
@@ -117,7 +117,7 @@ impl<'a> PolyeqElaborator<'a> {
                         let assigment_args: Vec<_> = a_bindings
                             .iter()
                             .zip(b_bindings)
-                            .map(|((a_var, _), b)| (a_var.clone(), pool.add(b.clone().into())))
+                            .map(|((a_var, a_sort), b)| ((a_var.clone(), a_sort.clone()), pool.add(b.clone().into())))
                             .collect();
 
                         let new_context_id = c.force_new_context();
@@ -338,7 +338,7 @@ impl<'a> PolyeqElaborator<'a> {
 
     fn close_subproof(
         &mut self,
-        assignment_args: Vec<(String, Rc<Term>)>,
+        assignment_args: Vec<(SortedVar, Rc<Term>)>,
         variable_args: Vec<SortedVar>,
         end_step: ProofStep,
     ) -> (usize, usize) {

carcara/src/parser/mod.rs
@@ -109,7 +109,7 @@ struct SortDef {
 /// the final AST.
 struct AnchorCommand {
     end_step_id: String,
-    assignment_args: Vec<(String, Rc<Term>)>,
+    assignment_args: Vec<(SortedVar, Rc<Term>)>,
     variable_args: Vec<SortedVar>,
 }
 
@@ -996,7 +996,7 @@ impl<'a, R: BufRead> Parser<'a, R> {
             let args = self.parse_sequence(Parser::parse_anchor_argument, true)?;
             for a in args {
                 match a {
-                    AnchorArg::Assign(var, value) => assignment_args.push((var.clone(), value)),
+                    AnchorArg::Assign(var, value) => assignment_args.push(((var.clone(), self.pool.sort(&value)), value)),
                     AnchorArg::Variable(var) => variable_args.push(var.clone()),
                 }
             }