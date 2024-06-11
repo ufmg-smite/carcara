0601092da1c784166d51f7be6a09c6240b08d8d5
Bruno Andreotti <bandreotti@proton.me>
2024-06-11 16:58:50 +0200

    Refactor resolution uncrowding module

--- carcara/src/elaborator/uncrowding.rs
+++ carcara/src/elaborator/uncrowding.rs
@@ -6,16 +6,38 @@ fn literals_to_clause(pool: &mut dyn TermPool, clause: &[Literal]) -> Vec<Rc<Ter
     clause.iter().map(|l| literal_to_term(pool, *l)).collect()
 }
 
-fn apply_naive_resolution<'a>(
-    premises: &[Vec<Literal<'a>>],
-    pivots: &[(Literal, bool)],
-) -> Vec<Literal<'a>> {
+struct ResolutionPremise<'a> {
+    node: Rc<ProofNode>,
+    clause: Vec<Literal<'a>>,
+    pivot: Option<(Literal<'a>, bool)>,
+}
+
+impl<'a> ResolutionPremise<'a> {
+    fn from_step(step: &'a StepNode) -> Vec<Self> {
+        let pivots = std::iter::once(None).chain(step.args.chunks(2).map(|chunk| {
+            let pivot = chunk[0].as_term().unwrap().remove_all_negations();
+            let polarity = chunk[1].as_term().unwrap().is_bool_true();
+            Some((pivot, polarity))
+        }));
+        step.premises
+            .iter()
+            .zip(pivots)
+            .map(|(node, pivot)| {
+                let clause = node.clause().iter().map(Rc::remove_all_negations).collect();
+                ResolutionPremise { node: node.clone(), clause, pivot }
+            })
+            .collect()
+    }
+}
+
+fn apply_naive_resolution<'a>(premises: &[ResolutionPremise<'a>]) -> Vec<Literal<'a>> {
     assert!(premises.len() >= 2);
-    assert_eq!(pivots.len(), premises.len() - 1);
 
-    let mut current = premises[0].clone();
+    let mut current = premises[0].clause.clone();
 
-    for (premise, &(pivot, polarity)) in premises[1..].iter().zip(pivots) {
+    for ResolutionPremise { clause, pivot, .. } in &premises[1..] {
+        let (pivot, polarity) = pivot.unwrap();
+
         let negated_pivot = (pivot.0 + 1, pivot.1);
         let (pivot_in_current, pivot_in_next) = if polarity {
             (pivot, negated_pivot)
@@ -27,7 +49,7 @@ fn apply_naive_resolution<'a>(
         current.remove(pos);
 
         let mut found = false;
-        for &t in premise {
+        for &t in clause {
             if !found && t == pivot_in_next {
                 found = true;
             } else {
@@ -43,65 +65,35 @@ fn apply_naive_resolution<'a>(
 pub fn uncrowd_resolution(pool: &mut PrimitivePool, step: &StepNode) -> Rc<ProofNode> {
     let target_conclusion: HashSet<_> = step.clause.iter().map(Rc::remove_all_negations).collect();
 
-    let premise_clauses: Vec<Vec<_>> = step
-        .premises
-        .iter()
-        .map(|p| p.clause().iter().map(Rc::remove_all_negations).collect())
-        .collect();
-    let pivots: Vec<_> = step
-        .args
-        .chunks(2)
-        .map(|chunk| {
-            let pivot = chunk[0].as_term().unwrap().remove_all_negations();
-            let polarity = chunk[1].as_term().unwrap().is_bool_true();
-            (pivot, polarity)
-        })
-        .collect();
-    let naive_conclusion = apply_naive_resolution(&premise_clauses, &pivots);
+    let mut premises = ResolutionPremise::from_step(step);
 
-    let crowding_literals = find_crowding_literals(
-        &naive_conclusion,
-        &target_conclusion,
-        &premise_clauses,
-        &pivots,
-    );
-    let mut contractions = find_needed_contractions(crowding_literals);
-    if contractions.last() != Some(&step.premises.len()) {
-        contractions.push(step.premises.len());
+    let naive_conclusion = apply_naive_resolution(&premises);
+
+    let literals_info = find_crowding_literals(&naive_conclusion, &target_conclusion, &premises);
+
+    // TODO: reorder premises
+
+    let mut contractions = find_needed_contractions(literals_info);
+    if contractions.last() != Some(&premises.len()) {
+        contractions.push(premises.len());
     }
 
     let mut previous_cut = 0;
-    let mut previous_node = None;
-    let mut previous_clause = None;
-    let mut pivots = pivots.into_iter();
     let mut ids = IdHelper::new(&step.id);
 
     for cut in contractions {
-        let premise_nodes: Vec<_> = previous_node
-            .iter()
-            .chain(&step.premises[previous_cut..cut])
-            .cloned()
-            .collect();
-        let clauses: Vec<_> = previous_clause
-            .into_iter()
-            .chain(premise_clauses[previous_cut..cut].iter().cloned())
-            .collect();
-        let pivots: Vec<_> = (&mut pivots).take(premise_nodes.len() - 1).collect();
         let (node, clause) = add_partial_resolution_step(
             pool,
             &mut ids,
             step.depth,
-            premise_nodes,
-            clauses,
-            pivots,
+            &premises[previous_cut..cut],
             &step.clause,
         );
-        previous_cut = cut;
-        previous_node = Some(node);
-        previous_clause = Some(clause);
+        premises[cut - 1] = ResolutionPremise { node, clause, pivot: None };
+        previous_cut = cut - 1;
     }
 
-    let mut final_step = previous_node.unwrap().as_step().unwrap().clone();
+    let mut final_step = premises[previous_cut].node.as_step().unwrap().clone();
 
     if final_step.clause.len() != step.clause.len() {
         let clause = get_or_intro_clause(&final_step.clause, &step.clause);
@@ -128,7 +120,7 @@ pub fn uncrowd_resolution(pool: &mut PrimitivePool, step: &StepNode) -> Rc<Proof
         }
     }
 
-    // To make sure elaboration is idempotent, we need to change the last id match the original
+    // To make sure elaboration is idempotent, we need to change the last id to match the original
     // step's id
     final_step.id = step.id.clone();
     Rc::new(ProofNode::Step(final_step))
@@ -138,43 +130,40 @@ fn add_partial_resolution_step<'a>(
     pool: &mut dyn TermPool,
     ids: &mut IdHelper,
     depth: usize,
-    premises: Vec<Rc<ProofNode>>,
-    premise_clauses: Vec<Vec<Literal<'a>>>,
-    pivots: Vec<(Literal<'a>, bool)>,
+    premises: &[ResolutionPremise<'a>],
     final_target: &[Rc<Term>],
 ) -> (Rc<ProofNode>, Vec<Literal<'a>>) {
-    let conclusion = apply_naive_resolution(&premise_clauses, &pivots);
+    let conclusion = apply_naive_resolution(premises);
     let contracted_conclusion: Vec<_> = conclusion.iter().dedup().copied().collect();
 
     let needs_contraction = contracted_conclusion.len() != conclusion.len();
 
-    let args = pivots
-        .into_iter()
-        .flat_map(|(literal, polarity)| {
+    let args: Vec<_> = premises[1..]
+        .iter()
+        .flat_map(|p| {
+            let (literal, polarity) = p.pivot.unwrap();
             [literal_to_term(pool, literal), pool.bool_constant(polarity)]
         })
         .map(ProofArg::Term)
         .collect();
 
-    let clause = literals_to_clause(pool, &conclusion);
-    let contracted_clause = clause.iter().dedup().cloned().collect();
-
     let resolution_step = Rc::new(ProofNode::Step(StepNode {
         id: ids.next_id(),
         depth,
-        clause,
+        clause: literals_to_clause(pool, &conclusion),
         rule: "resolution".to_owned(),
-        premises,
+        premises: premises.iter().map(|p| p.node.clone()).collect(),
         args,
         discharge: Vec::new(),
         previous_step: None,
     }));
 
     if resolution_step.clause() == final_target {
-        return (resolution_step, contracted_conclusion);
+        return (resolution_step, conclusion);
     }
 
     if needs_contraction {
+        let contracted_clause = resolution_step.clause().iter().dedup().cloned().collect();
         let contraction_step = Rc::new(ProofNode::Step(StepNode {
             id: ids.next_id(),
             depth,
@@ -215,57 +204,67 @@ fn get_or_intro_clause(current: &[Rc<Term>], target: &[Rc<Term>]) -> Vec<Rc<Term
     result
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct CrowdingLiteralInfo {
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+struct LiteralInfo {
+    is_crowding: bool,
     last_inclusion: usize,
     eliminator: usize,
+
+    // Polarity with which the crowding literal appears on the left-hand clause. I.e., `true` if the
+    // eliminator literal is the crowding literal +1 negation, `false` if it is -1 negation
+    polarity: bool,
 }
 
 fn find_crowding_literals<'a>(
     naive_conclusion: &[Literal<'a>],
     target_conclusion: &HashSet<Literal<'a>>,
-    premises: &[Vec<Literal<'a>>],
-    pivots: &[(Literal<'a>, bool)],
-) -> HashMap<Literal<'a>, CrowdingLiteralInfo> {
-    let mut crowding_lits: HashMap<Literal, CrowdingLiteralInfo> = naive_conclusion
+    premises: &[ResolutionPremise<'a>],
+) -> HashMap<Literal<'a>, LiteralInfo> {
+    let mut literals: HashMap<_, _> = premises
         .iter()
-        .filter(|lit| !target_conclusion.contains(lit))
-        .map(|&l| (l, CrowdingLiteralInfo { last_inclusion: 0, eliminator: 0 }))
+        .flat_map(|p| &p.clause)
+        .map(|l| (*l, LiteralInfo::default()))
         .collect();
 
-    for (i, clause) in premises.iter().enumerate() {
-        for lit in clause {
-            if let Some(info) = crowding_lits.get_mut(lit) {
+    naive_conclusion
+        .iter()
+        .filter(|lit| !target_conclusion.contains(lit))
+        .for_each(|lit| literals.get_mut(lit).unwrap().is_crowding = true);
+
+    for (i, p) in premises.iter().enumerate() {
+        for lit in &p.clause {
+            if let Some(info) = literals.get_mut(lit) {
                 info.last_inclusion = i;
             }
         }
     }
-    for (i, &(pivot, polarity)) in pivots.iter().enumerate() {
+    for (i, p) in premises[1..].iter().enumerate() {
+        let (pivot, polarity) = p.pivot.unwrap();
         let pivot_in_current = if polarity {
             pivot
         } else {
             (pivot.0 + 1, pivot.1)
         };
-        if let Some(info) = crowding_lits.get_mut(&pivot_in_current) {
+        if let Some(info) = literals.get_mut(&pivot_in_current) {
             if i + 1 > info.last_inclusion {
                 info.eliminator = i + 1;
+                info.polarity = polarity;
             }
         }
     }
-    crowding_lits
+    literals
 }
 
-fn find_needed_contractions(
-    crowding_literals: HashMap<Literal, CrowdingLiteralInfo>,
-) -> Vec<usize> {
+fn find_needed_contractions(literals_info: HashMap<Literal, LiteralInfo>) -> Vec<usize> {
     #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
     enum Event {
         Elimination,
         LastInclusion,
     }
 
-    let mut events: Vec<_> = crowding_literals
+    let mut events: Vec<_> = literals_info
         .iter()
+        .filter(|(_, info)| info.is_crowding)
         .flat_map(|(lit, info)| {
             [
                 (*lit, Event::LastInclusion, info.last_inclusion),
@@ -303,83 +302,7 @@ fn find_needed_contractions(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::parser::{self, parse_instance, parse_instance_with_pool, tests::*};
-
-    fn parse_premises(
-        pool: &mut PrimitivePool,
-        definitions: &str,
-        premises: Vec<Vec<&str>>,
-    ) -> Vec<Vec<Rc<Term>>> {
-        premises
-            .into_iter()
-            .map(|c| {
-                c.iter()
-                    .flat_map(|t| parse_terms(pool, definitions, [t]))
-                    .collect()
-            })
-            .collect()
-    }
-
-    fn premises_to_literals(premises: &[Vec<Rc<Term>>]) -> Vec<Vec<Literal>> {
-        premises
-            .iter()
-            .map(|c| c.iter().map(Rc::remove_all_negations).collect())
-            .collect()
-    }
-
-    #[test]
-    fn test_find_needed_contractions() {
-        let mut pool = PrimitivePool::new();
-        let premises = vec![
-            vec!["x", "a", "b"],
-            vec!["(not x)", "y", "a", "c"],
-            vec!["(not y)", "z", "b"],
-            vec!["(not a)"],
-            vec!["(not z)", "c"],
-            vec!["d", "(not b)"],
-            vec!["d", "(not c)"],
-            vec!["(not d)"],
-        ];
-        let definitions = "
-            (declare-const a Bool)
-            (declare-const b Bool)
-            (declare-const c Bool)
-            (declare-const d Bool)
-            (declare-const x Bool)
-            (declare-const y Bool)
-            (declare-const z Bool)
-        ";
-        let premises = parse_premises(&mut pool, definitions, premises);
-        let premises = premises_to_literals(&premises);
-        let [a, b, c, d, x, y, z] = [
-            premises[1][2],
-            premises[0][2],
-            premises[1][3],
-            premises[5][0],
-            premises[0][0],
-            premises[1][1],
-            premises[2][1],
-        ];
-        let pivots = [x, y, a, z, b, c, d].map(|lit| (lit, true));
-
-        let naive_conclusion = apply_naive_resolution(&premises, &pivots);
-        let target_conclusion = HashSet::new();
-
-        let crowding_literals =
-            find_crowding_literals(&naive_conclusion, &target_conclusion, &premises, &pivots);
-
-        let expected = [
-            (a, CrowdingLiteralInfo { last_inclusion: 1, eliminator: 3 }),
-            (b, CrowdingLiteralInfo { last_inclusion: 2, eliminator: 5 }),
-            (c, CrowdingLiteralInfo { last_inclusion: 4, eliminator: 6 }),
-            (d, CrowdingLiteralInfo { last_inclusion: 6, eliminator: 7 }),
-        ]
-        .into_iter()
-        .collect();
-        assert_eq!(crowding_literals, expected);
-
-        assert_eq!(find_needed_contractions(crowding_literals), [3, 6, 7]);
-    }
+    use crate::parser::{self, parse_instance, parse_instance_with_pool};
 
     #[test]
     fn test_uncrowd_resolution() {
