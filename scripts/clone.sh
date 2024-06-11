dfd70bde1a45578d728e42a2ca81115562a19972
Bruno Andreotti <bandreotti@proton.me>
2024-06-11 15:31:37 +0200

    Adapt `generate-benchmarks.sh` to also use cvc5

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -2,12 +2,12 @@
 
 mkdir -p benchmarks/full
 
+cd benchmarks || exit 1
+
 for logic in AUFLIA AUFLIRA QF_ALIA QF_AUFLIA QF_IDL QF_LIA QF_LRA QF_RDL \
     QF_UF QF_UFIDL QF_UFLIA QF_UFLRA UF UFIDL UFLIA UFLRA; do
-    if [ ! -d benchmarks/full/$logic ]; then
-        git clone --depth 1 \
-            https://clc-gitlab.cs.uiowa.edu:2443/SMT-LIB-benchmarks/$logic.git \
-            benchmarks/full/$logic || exit 1
-        rm -rf benchmarks/full/$logic/.git
+    if [ ! -d full/$logic ]; then
+        curl "https://zenodo.org/records/11061097/files/${logic}.tar.zst" \
+            | tar --zstd -x -C full --strip-components 1
     fi
 done
