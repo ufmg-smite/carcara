0fcb5ad9a1de56cf8b4a820be89a13493ab0c7ab
Haniel Barbosa <hanielbbarbosa@gmail.com>
2024-06-17 11:05:38 -0300

    minor changes from meeting

--- carcara/src/checker/mod.rs
+++ carcara/src/checker/mod.rs
@@ -558,7 +558,7 @@ impl<'c> ProofChecker<'c> {
             "concat_lprop_suffix" => strings::concat_lprop_suffix,
 
             // Drat formar rules
-            "drat" => drat::resolution,
+            "drat" => drat::drat,
 
 
             // Special rules that always check as valid, and are used to indicate holes in the
--- carcara/src/checker/rules/drat.rs
+++ carcara/src/checker/rules/drat.rs
@@ -3,7 +3,20 @@ use super::{
     CheckerError, Premise, RuleArgs, RuleResult,
 };
 
-pub fn resolution(rule_args: RuleArgs) -> RuleResult {
-    let RuleArgs { conclusion, premises, .. } = rule_args;
+fn rup(conclusion: &[Rc<Term>], clause_set: Vec<IndexSet<(bool, &Rc<Term>)>>) -> bool {
+  // TODO
+}
 
+pub fn drat( RuleArgs { conclusion, premises, args, .. } : RuleArgs) -> RuleResult {
+    assert_clause_len(conclusion, 0)?;
+    // - the initial clause set is the premises (build it as what is
+    //   expected in the above function)
+    // - for each argument:
+    //   - if it is *not* a deletion, then show that you can conclude
+    //     the respective clause from the current clause set via
+    //     RUP. Add the argument to the clause set.
+    //   - if it is a deletion: delete that clause from the clause_set
+    // - Once all arguments have been processed, you are done as long
+    //   as the empty clause has been added to the clause set.
+    Ok(())
 }
