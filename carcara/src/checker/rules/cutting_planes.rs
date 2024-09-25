8b8f6cdff0c1ca192fc977094c5834f963469661
bernborgess <bernborgess@outlook.com>
2024-09-25 10:43:31 -0300

    Add cp_division checker (partial)

--- carcara/src/checker/rules/cutting_planes.rs
+++ carcara/src/checker/rules/cutting_planes.rs
@@ -112,10 +112,46 @@ pub fn cp_multiplication(RuleArgs { premises, args, conclusion, .. }: RuleArgs) 
 }
 
 pub fn cp_division(RuleArgs { premises, args, conclusion, .. }: RuleArgs) -> RuleResult {
-    println!("Division");
     assert_num_premises(premises, 1)?;
+    let clause = &premises[0].clause[0];
+
+    // Check there is exactly one arg
     assert_num_args(args, 1)?;
+    let divisor: Integer = args[0].as_term()?.as_integer_err()?;
+
+    // Check there is exacly one conclusion
     assert_clause_len(conclusion, 1)?;
+    let conclusion = &conclusion[0];
+
+    // Unwrap the premise inequality
+    let (pbsum_p, constant_p) = match_term_err!((>= (+ ...) constant) = clause)?;
+    let constant_p = constant_p.as_integer_err()?;
+    let pbsum_p = get_pb_hashmap(pbsum_p)?;
+
+    // Unwrap the conclusion inequality
+    let (pbsum_c, constant_c) = match_term_err!((>= (+ ...) constant_c) = conclusion)?;
+    let constant_c = constant_c.as_integer_err()?;
+    let pbsum_c = get_pb_hashmap(pbsum_c)?;
+
+    // Verify constants match
+    // TODO: CeilDiv
+    rassert!(
+        constant_p.clone() / divisor.clone() == constant_c,
+        CheckerError::ExpectedInteger(constant_p / divisor.clone(), conclusion.clone())
+    );
+
+    // Verify pseudo-boolean sums match
+    for (literal, coeff_p) in pbsum_p {
+        if let Some(coeff_c) = pbsum_c.get(&literal) {
+            // TODO: CeilDiv
+            let expected = coeff_p / &divisor;
+            rassert!(
+                &expected == coeff_c,
+                CheckerError::ExpectedInteger(expected, conclusion.clone())
+            );
+        }
+    }
+
     Ok(())
 }
 
@@ -124,7 +160,7 @@ pub fn cp_saturation(RuleArgs { premises, args, conclusion, .. }: RuleArgs) -> R
     assert_num_args(args, 0)?;
     let clause = &premises[0].clause[0];
 
-    // Check there is exacly one clonclusion
+    // Check there is exacly one conclusion
     assert_clause_len(conclusion, 1)?;
     let conclusion = &conclusion[0];
 
@@ -223,8 +259,14 @@ mod tests {
                 (declare-fun x1 () Int)
                 ",
             "Simple working examples" {
-                r#"(assume c1 (>= (* 2 x1) 2))
-                   (step t1 (cl (>= x1 1)) :rule cp_division :premises (c1) :args (2) )"#: true,
+                r#"(assume c1 (>= (+ (* 2 x1) 0) 2))
+                   (step t1 (cl (>= (+ (* 1 x1) 0) 1)) :rule cp_division :premises (c1) :args (2) )"#: true,
+            }
+            "Wrong division" {
+                r#"(assume c1 (>= (+ (* 2 x1) 0) 2))
+                   (step t1 (cl (>= (+ (* 2 x1) 0) 1)) :rule cp_division :premises (c1) :args (2) )"#: false,
+                r#"(assume c1 (>= (+ (* 2 x1) 0) 2))
+                   (step t1 (cl (>= (+ (* 1 x1) 0) 2)) :rule cp_division :premises (c1) :args (2) )"#: false,
             }
         }
     }
