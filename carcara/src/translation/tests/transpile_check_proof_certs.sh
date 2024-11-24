914fc1815b78655a6c13d89d39131e7fec633d27
Mallku2 <mallkuernesto@gmail.com>
2024-11-24 20:39:49 -0300

    add a checking step with carcara, in gen_proof_certs.sh; using release version of carcara in both gen and check scripts

--- carcara/src/translation/tests/transpile_check_proof_certs.sh
+++ carcara/src/translation/tests/transpile_check_proof_certs.sh
@@ -4,7 +4,7 @@
 # contained in $DIR. Uses $COMMAND to invoke 'carcara'.
 DIR="./problems/"
 # TODO: installation-dependent relative paths
-CARCARA_COMMAND="../../../../target/debug/carcara translate"
+CARCARA_COMMAND="../../../../target/release/carcara translate"
 ETHOS_COMMAND="../../../../../ethos_fork/ethos/build/src/ethos"
 
 # Loop through each file in the directory.
