f0de158af3f0a2772125777231461287819a9d14
bernborgess <bernborgess@outlook.com>
2025-04-11 17:28:56 -0300

    Refactor out the `range` value

--- carcara/src/checker/rules/pb_blasting.rs
+++ carcara/src/checker/rules/pb_blasting.rs
@@ -4,7 +4,6 @@ use crate::{
     checker::error::CheckerError,
 };
 use rug::Integer;
-use std::ops::Range;
 
 /// Helper to get the bit width of a bitvector looking into the pool
 fn get_bit_width(x: &Rc<Term>, pool: &mut dyn TermPool) -> Result<usize, CheckerError> {
@@ -28,17 +27,14 @@ fn get_pbsum(pbsum: &Rc<Term>) -> &[Rc<Term>] {
     }
 }
 
-/// Helper to check that a summation has the expected shape
+// Helper to check that a summation has the expected shape
 fn check_pbblast_sum(
     pool: &mut dyn TermPool,
     bitvector: &Rc<Term>,
     sum: &[Rc<Term>],
-    range: &Range<usize>,
 ) -> RuleResult {
     // Obtain the bitvector width from the pool.
     let width = get_bit_width(bitvector, pool)?;
-
-    // The `range` must be the same length as the `sum`, but may be less than `width`
 
     // The summation must have at most as many summands as the bitvector has bits.
     rassert!(
@@ -50,17 +46,7 @@ fn check_pbblast_sum(
         ))
     );
 
-    // The summation must have as many summands as the range has element.
-    rassert!(
-        range.len() == sum.len(),
-        CheckerError::Explanation(format!(
-            "Mismatched range size {} {}",
-            range.len(),
-            sum.len()
-        ))
-    );
-
-    for (i, element) in range.clone().zip(sum.iter()) {
+    for (i, element) in sum.iter().enumerate() {
         // Try to match (* c ((_ int_of idx) bv))
         let (c, idx, bv) = match match_term!((* c ((_ int_of idx) bv)) = element) {
             Some((c, (idx, bv))) => (c.as_integer_err()?, idx, bv),
@@ -84,7 +70,7 @@ fn check_pbblast_sum(
         };
 
         // Convert the index term to an integer.
-        let idx = idx.as_integer_err()?;
+        let idx: Integer = idx.as_integer_err()?;
         // Check that the coefficient is 2^i.
         rassert!(
             c == (Integer::from(1) << i),
@@ -113,17 +99,15 @@ fn check_pbblast_constraint(
     right_bv: &Rc<Term>,
     left_sum: &[Rc<Term>],
     right_sum: &[Rc<Term>],
-    range: Option<Range<usize>>,
 ) -> RuleResult {
-    let range = range.unwrap_or(0..(left_sum.len()));
-    check_pbblast_sum(pool, left_bv, left_sum, &range)?;
-    check_pbblast_sum(pool, right_bv, right_sum, &range)
+    check_pbblast_sum(pool, left_bv, left_sum)?;
+    check_pbblast_sum(pool, right_bv, right_sum)
 }
 
 /// Implements the equality rule
 /// The expected shape is:
 ///    `(= (= x y) (= (- (+ sum_x) (+ sum_y)) 0))`
-pub fn pbblast_bveq(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
+pub fn pbblast_bveq(RuleArgs { pool, conclusion, .. }: RuleArgs) -> RuleResult {
     let ((x, y), ((sum_x, sum_y), constant)) =
         match_term_err!((= (= x y) (= (- sum_x sum_y) constant)) = &conclusion[0])?;
 
@@ -132,7 +116,7 @@ pub fn pbblast_bveq(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
     let sum_y = get_pbsum(sum_y);
 
     // Check that the constant is 0
-    let constant = constant.as_integer_err()?;
+    let constant: Integer = constant.as_integer_err()?;
     rassert!(
         constant == 0,
         CheckerError::Explanation(format!("Non-zero constant {}", constant))
@@ -140,13 +124,13 @@ pub fn pbblast_bveq(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
 
     // Check that the summations have the correct structure.
     // (For equality the order is: sum_x for x and sum_y for y.)
-    check_pbblast_constraint(pool, x, y, sum_x, sum_y, None)
+    check_pbblast_constraint(pool, x, y, sum_x, sum_y)
 }
 
 /// Implements the unsigned-less-than rule.
 /// The expected shape is:
 ///    `(= (bvult x y) (>= (- (+ sum_y) (+ sum_x)) 1))`
-pub fn pbblast_bvult(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult {
+pub fn pbblast_bvult(RuleArgs { pool, conclusion, .. }: RuleArgs) -> RuleResult {
     let ((x, y), ((sum_y, sum_x), constant)) =
         match_term_err!((= (bvult x y) (>= (- sum_y sum_x) constant)) = &conclusion[0])?;
 
@@ -162,7 +146,7 @@ pub fn pbblast_bvult(RuleArgs { conclusion, pool, .. }: RuleArgs) -> RuleResult 
     );
 
     // For bvult the summations occur in reverse: the "left" sum comes from y and the "right" from x.
-    check_pbblast_constraint(pool, y, x, sum_y, sum_x, None)
+    check_pbblast_constraint(pool, y, x, sum_y, sum_x)
 }
 
 /// Implements the unsigned-greater-than rule.
