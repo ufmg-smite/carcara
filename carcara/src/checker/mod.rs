6f6b81df6748e15f394ba1d263e228130c0ef2a0
bernborgess <62393097+bernborgess@users.noreply.github.com>
2025-05-29 14:05:13 -0300

    Implement rule `cp_literal` (#88)
    * Declaration of rule `cp_literal`
    
    * Enumerate all valid patterns in `cp_literal`

--- carcara/src/checker/mod.rs
+++ carcara/src/checker/mod.rs
@@ -495,6 +495,7 @@ impl<'c> ProofChecker<'c> {
             "cp_multiplication" => cutting_planes::cp_multiplication,
             "cp_division" => cutting_planes::cp_division,
             "cp_saturation" => cutting_planes::cp_saturation,
+            "cp_literal" => cutting_planes::cp_literal,
 
             "string_decompose" => strings::string_decompose,
             "string_length_pos" => strings::string_length_pos,
