d505b42488d1329a82ac88e0fa7e4dcc886c946d
bernborgess <62393097+bernborgess@users.noreply.github.com>
2025-05-29 14:05:13 -0300

    Implement rule `cp_literal` (#88)
    * Declaration of rule `cp_literal`
    
    * Enumerate all valid patterns in `cp_literal`

--- carcara/tests/rules/cutting_planes.rs
+++ carcara/tests/rules/cutting_planes.rs
@@ -279,3 +279,35 @@ fn cp_saturation() {
 
     }
 }
+
+#[test]
+fn cp_literal() {
+    test_cases! {
+        definitions = "
+            (declare-fun l () Int)
+            (define-fun neg_l () Int (- 1 l))
+        ",
+        "cp_literal correctly applied" {
+            r#"(step t1 (cl (>= l 0)) :rule cp_literal :args (l))"#: true,
+            r#"(step t1 (cl (>= (* 1 l) 0)) :rule cp_literal :args (l))"#: true,
+            r#"(step t1 (cl (>= neg_l 0)) :rule cp_literal :args (neg_l))"#: true,
+            r#"(step t1 (cl (>= (* 1 neg_l) 0)) :rule cp_literal :args (neg_l))"#: true,
+        }
+        "cp_literal invalid (coefficients)" {
+            r#"(step t1 (cl (>= (* 1 l) 0)) :rule cp_literal :args ((* 2 l)))"#: false,
+            r#"(step t1 (cl (>= (* 2 l) 0)) :rule cp_literal :args ((* 2 l)))"#: false,
+
+            r#"(step t1 (cl (>= (* 1 neg_l) 0)) :rule cp_literal :args ((* 2 neg_l)))"#: false,
+            r#"(step t1 (cl (>= (* 2 neg_l) 0)) :rule cp_literal :args ((* 2 neg_l)))"#: false,
+
+            // ! THIS SHOULD BE AVOIDED WHEN l is a PSEUDO BOOLEAN
+            r#"(step t1 (cl (>= (* 1 (* 2 l)) 0)) :rule cp_literal :args ((* 2 l)))"#: true,
+            r#"(step t1 (cl (>= (* 1 (* 2 neg_l)) 0)) :rule cp_literal :args ((* 2 neg_l)))"#: true,
+        }
+        "cp_literal invalid (number of args)" {
+            r#"(step t1 (cl (>= (* 1 l) 0)) :rule cp_literal)"#: false,
+            r#"(step t1 (cl (>= (* 1 l) 0)) :rule cp_literal :args (l neg_l))"#: false,
+            r#"(step t1 (cl (>= (* 1 neg_l) 0)) :rule cp_literal :args (neg_l l))"#: false,
+        }
+    }
+}
