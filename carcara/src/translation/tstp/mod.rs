ecd87042678a216c8a47f69a8a14b10de59017dd
Mallku2 <mallkuernesto@gmail.com>
2025-07-01 20:27:17 -0300

    alethe2tstp: fixes in printer

--- carcara/src/translation/tstp/mod.rs
+++ carcara/src/translation/tstp/mod.rs
@@ -1,4 +1,3 @@
 pub mod alethe_2_tstp;
+pub mod printer;
 pub mod tstp_ast;
-// pub mod printer;
-// pub mod tests;
---
+++ carcara/src/translation/tstp/printer.rs
@@ -0,0 +1,186 @@
+//! A pretty printer for Tstp proofs.
+use crate::translation::tstp::tstp_ast::*;
+use std::io;
+
+// TODO: struct for future actual formatting concerns
+// TODO: change this to a proper formatter for TSTP
+/// A formatter for S-expressions.
+pub struct SExpFormatter<'a> {
+    sink: &'a mut dyn io::Write,
+}
+
+impl<'a> SExpFormatter<'a> {
+    pub fn new(sink: &'a mut dyn io::Write) -> Self {
+        SExpFormatter { sink }
+    }
+
+    // Prints an s-expression with properly formatted concrete syntax, and
+    // separating it from surrounding s-expressions.
+    fn write_s_expr(&mut self, tag: &str, args: &[String]) -> io::Result<()> {
+        if args.is_empty() {
+            // S-expression is a constant
+            write!(self.sink, "{}", tag)?;
+        } else {
+            // {not args.is_empty()}
+            // S-expression has the form (tag arg1 ...)
+            write!(self.sink, "(")?;
+            write!(self.sink, "{}", tag)?;
+            // TODO: how to propagate errors from within the lambda abstraction?
+            args.iter().for_each(|arg| {
+                let _ = write!(self.sink, " {}", arg);
+            });
+
+            write!(self.sink, ")")?;
+        };
+
+        writeln!(self.sink)?;
+
+        Ok(())
+    }
+}
+
+pub struct TstpPrinter<'a> {
+    // Where to write.
+    // TODO: use something different that an SExpFormatter
+    formatted_sink: SExpFormatter<'a>,
+}
+
+pub trait PrintProof {
+    fn write_proof(&mut self, proof: &TstpProof) -> io::Result<()>;
+}
+
+impl<'a> PrintProof for TstpPrinter<'a> {
+    /// Formatted proof printing.
+    fn write_proof(&mut self, proof: &TstpProof) -> io::Result<()> {
+        let mut tag: String;
+        let mut args: Vec<String>;
+
+        // TODO: some generic way of doing this? maybe with macros?
+        for command in proof {
+            let TstpAnnotatedFormula {
+                language,
+                name,
+                role,
+                formula,
+                source: _,
+                useful_info: _,
+            } = command;
+            {
+                // TODO: polish this with a proper formatted sink
+                let _ = self
+                    .formatted_sink
+                    .write_s_expr(&TstpPrinter::language_to_concrete_syntax(language), &[]);
+                tag = name.clone();
+                args = vec![
+                    TstpPrinter::role_to_concrete_syntax(role),
+                    TstpPrinter::formula_to_concrete_syntax(formula),
+                    // TODO: discarding source and useful_info
+                    "".to_owned(),
+                    "".to_owned(),
+                ];
+                self.formatted_sink.write_s_expr(&tag, &args)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl<'a> TstpPrinter<'a> {
+    pub fn new(dest: SExpFormatter<'a>) -> Self {
+        Self { formatted_sink: dest }
+    }
+
+    fn language_to_concrete_syntax(language: &TstpLanguage) -> String {
+        match language {
+            TstpLanguage::Fof => String::from("fof"),
+
+            TstpLanguage::Tff => String::from("tff"),
+        }
+    }
+
+    fn role_to_concrete_syntax(role: &TstpFormulaRole) -> String {
+        match role {
+            TstpFormulaRole::Axiom => "axiom".to_owned(),
+
+            TstpFormulaRole::Lemma => "lemma".to_owned(),
+
+            TstpFormulaRole::Conjecture => "conjecture".to_owned(),
+
+            TstpFormulaRole::Hypothesis => "hypothesis".to_owned(),
+
+            TstpFormulaRole::Logic => "logic".to_owned(),
+
+            TstpFormulaRole::Type => "type".to_owned(),
+        }
+    }
+
+    fn formula_to_concrete_syntax(term: &TstpFormula) -> String {
+        let mut ret;
+
+        match term {
+            TstpFormula::Typing(var, type_inhabited) => {
+                ret = TstpPrinter::formula_to_concrete_syntax(var);
+                ret += &(": ".to_owned() + &TstpPrinter::type_to_concrete_syntax(type_inhabited));
+            }
+
+            TstpFormula::Variable(name) => {
+                ret = name.clone();
+            }
+
+            _ => {
+                panic!();
+            }
+        }
+
+        ret
+    }
+
+    fn type_to_concrete_syntax(type_term: &TstpType) -> String {
+        match type_term {
+            TstpType::UserDefined(name) => "\'".to_owned() + &name.clone() + "\'",
+
+            TstpType::Bool => "$o".to_owned(),
+
+            TstpType::Fun(domain, codomain) => {
+                let mut ret = "( ".to_owned();
+
+                domain
+                    .iter()
+                    .for_each(|elem| ret += &TstpPrinter::type_to_concrete_syntax(elem));
+
+                // TODO: hard-coding here the arrow symbol
+                ret += " ) > ";
+
+                ret += &TstpPrinter::type_to_concrete_syntax(codomain);
+
+                ret
+            }
+
+            TstpType::Universe => "$tType".to_owned(),
+
+            _ => {
+                panic!();
+            }
+        }
+    }
+
+    // fn operator_to_concrete_syntax(op: &EunoiaOperator) -> String {
+    //     match op {
+    //         EunoiaOperator::Xor => "xor".to_owned(),
+
+    //         EunoiaOperator::Not => "not".to_owned(),
+
+    //         // NOTE: these are the symbols used in theory.eo
+    //         EunoiaOperator::Eq => "=".to_owned(),
+
+    //         EunoiaOperator::GreaterThan => ">".to_owned(),
+
+    //         EunoiaOperator::GreaterEq => ">=".to_owned(),
+
+    //         EunoiaOperator::LessThan => "<".to_owned(),
+
+    //         EunoiaOperator::LessEq => "<=".to_owned(),
+    //     }
+    // }
+}
