ba9d8437881304c1601ff8d2d5841622708581bb
NotBad4U <lightplay8@gmail.com>
2025-11-04 11:13:16 +0100

    Simplify dag terms on root and fix arith polynom proof

--- carcara/src/lambdapi/simp.rs
+++ carcara/src/lambdapi/simp.rs
@@ -14,6 +14,15 @@ pub fn translate_rare_simp(
     let rule: String =
         unwrap_match!(**rare_rule, crate::ast::Term::Const(Constant::String(ref s)) => s.clone());
 
+    let mut simps: Vec<_> = if dag_terms.len() > 0 {
+        dag_terms
+            .into_iter()
+            .map(|s| ProofStep::Simplify(vec![s.into()]))
+            .collect_vec()
+    } else {
+        vec![]
+    };
+
     let mut rewrites = match rule.as_str() {
         "bool-and-true" => translate_bool_and_true(args),
         "bool-or-false" => translate_bool_or_false(args),
@@ -22,13 +31,13 @@ pub fn translate_rare_simp(
         "bool-impl-elim" => translate_bool_impl_elim(args),
         "bool-and-de-morgan" => translate_bool_and_de_morgan(args),
         "bool-or-de-morgan" => translate_bool_or_de_morgan(args),
-        "bool-double-not-elim" => translate_bool_double_not_elim(dag_terms),
-        "bool-implies-or-distrib" => translate_bool_implies_or_distrib(args, dag_terms),
-        "bool-impl-true1" => translate_bool_imp_true1(dag_terms),
-        "bool-impl-true2" => translate_bool_imp_true2(dag_terms),
-        "bool-impl-false1" => translate_bool_imp_false1(dag_terms),
-        "bool-impl-false2" => translate_bool_imp_false2(dag_terms),
-        "arith-poly-norm" => translate_arith_poly_norm(clause, dag_terms),
+        "bool-double-not-elim" => translate_bool_double_not_elim(),
+        "bool-implies-or-distrib" => translate_bool_implies_or_distrib(args),
+        "bool-impl-true1" => translate_bool_imp_true1(),
+        "bool-impl-true2" => translate_bool_imp_true2(),
+        "bool-impl-false1" => translate_bool_imp_false1(),
+        "bool-impl-false2" => translate_bool_imp_false2(),
+        "arith-poly-norm" => translate_arith_poly_norm(clause),
         "evaluate" => {
             let cl_first = clause.first().expect("evaluate can not be empty");
             match match_term!((= l r) = cl_first) {
@@ -56,6 +65,7 @@ pub fn translate_rare_simp(
 
     Proof(lambdapi! {
         apply "∨ᵢ₁";
+        inject(simps);
         inject(rewrites);
     })
 }
@@ -123,14 +133,8 @@ fn translate_bool_eq_false(dag_terms: HashSet<String>) -> Vec<ProofStep> {
 }
 
 /// (define-rule bool-impl-false1 ((t Bool)) (=> t false) (not t))
-fn translate_bool_imp_false1(dag_terms: HashSet<String>) -> Vec<ProofStep> {
+fn translate_bool_imp_false1() -> Vec<ProofStep> {
     let mut proof = vec![];
-
-    if dag_terms.len() > 0 {
-        dag_terms
-            .into_iter()
-            .for_each(|s| proof.push(ProofStep::Simplify(vec![s.into()])));
-    }
 
     proof.push(ProofStep::Rewrite(
         false,
@@ -145,14 +149,8 @@ fn translate_bool_imp_false1(dag_terms: HashSet<String>) -> Vec<ProofStep> {
 }
 
 /// (define-rule bool-impl-false2 ((t Bool)) (=> false t) true)
-fn translate_bool_imp_false2(dag_terms: HashSet<String>) -> Vec<ProofStep> {
+fn translate_bool_imp_false2() -> Vec<ProofStep> {
     let mut proof = vec![];
-
-    if dag_terms.len() > 0 {
-        dag_terms
-            .into_iter()
-            .for_each(|s| proof.push(ProofStep::Simplify(vec![s.into()])));
-    }
 
     proof.push(ProofStep::Rewrite(
         false,
@@ -167,14 +165,8 @@ fn translate_bool_imp_false2(dag_terms: HashSet<String>) -> Vec<ProofStep> {
 }
 
 /// (define-rule bool-impl-true1 ((t Bool)) (=> t true) true)
-fn translate_bool_imp_true1(dag_terms: HashSet<String>) -> Vec<ProofStep> {
+fn translate_bool_imp_true1() -> Vec<ProofStep> {
     let mut proof = vec![];
-
-    if dag_terms.len() > 0 {
-        dag_terms
-            .into_iter()
-            .for_each(|s| proof.push(ProofStep::Simplify(vec![s.into()])));
-    }
 
     proof.push(ProofStep::Rewrite(
         false,
@@ -189,14 +181,8 @@ fn translate_bool_imp_true1(dag_terms: HashSet<String>) -> Vec<ProofStep> {
 }
 
 /// (define-rule bool-impl-true2 ((t Bool)) (=> true t) t)
-fn translate_bool_imp_true2(dag_terms: HashSet<String>) -> Vec<ProofStep> {
+fn translate_bool_imp_true2() -> Vec<ProofStep> {
     let mut proof = vec![];
-
-    if dag_terms.len() > 0 {
-        dag_terms
-            .into_iter()
-            .for_each(|s| proof.push(ProofStep::Simplify(vec![s.into()])));
-    }
 
     proof.push(ProofStep::Rewrite(
         false,
@@ -220,14 +206,8 @@ fn translate_bool_imp_true2(dag_terms: HashSet<String>) -> Vec<ProofStep> {
 /// ```
 ///
 /// We simplify `p_*` all shared symbols otherwise the tactic `rewrite` does not work.
-fn translate_bool_double_not_elim(dag_terms: HashSet<String>) -> Vec<ProofStep> {
+fn translate_bool_double_not_elim() -> Vec<ProofStep> {
     let mut proof = vec![];
-
-    if dag_terms.len() > 0 {
-        dag_terms
-            .into_iter()
-            .for_each(|s| proof.push(ProofStep::Simplify(vec![s.into()])));
-    }
 
     proof.push(ProofStep::Rewrite(
         false,
@@ -290,35 +270,28 @@ fn translate_evaluate_bool() -> Vec<ProofStep> {
 /// ```
 ///
 /// We then would like to produce the script that re-use the normalise for `la_generic``.
+/// Note that we need to reify left side first to re-use the reification map for the right side.
+/// Otherwise, we can not prove the equality of expression such as `x + y ≡ y + x` because the reification map would be different (l = [x |-> 0, x |-> 1], r = [x |-> 1, x |-> 0]).
 ///
 /// ```
 /// have t50_t3 : π̇ (e1 = e2) ⟇ ▩) {
-///  apply ∨ᵢ₁;
-///  rewrite left .[x in x = _] reify_correct;
-///  rewrite left .[x in _ = x] reify_correct;
-///  set l ≔ reify e1;
-///  rewrite .[z in val z = _] eta_prod;
-///  rewrite left norm_correct (l ₁) (l ₂) ⊤ᵢ;
-///  set r ≔ reify e2;
-///  rewrite .[z in _ = val z] eta_prod;
-///  rewrite left norm_correct (r ₁) (r ₂) ⊤ᵢ;
-///  reflexivity
+///     apply ∨ᵢ₁;
+///     rewrite left  .[x in x = _] reify_correct;
+///     set l ≔ (reify e1);
+///     rewrite .[x in val x = _] eta_prod;
+///     rewrite left .[x in _ = x] (reify_correct_withenv (l ₂));
+///     rewrite .[x in _ = val x] eta_prod;
+///     rewrite left .[x in x = _] norm_correct;
+///     rewrite left .[x in _ = x] norm_correct;
+///     reflexivity;
 /// };
 /// ```
-fn translate_arith_poly_norm(
-    clause: &Vec<Rc<AletheTerm>>,
-    dag_terms: HashSet<String>,
-) -> Vec<ProofStep> {
+fn translate_arith_poly_norm(clause: &Vec<Rc<AletheTerm>>) -> Vec<ProofStep> {
     let mut proof = vec![];
 
     let l_set_id = "l";
-    let r_set_id = "r";
 
-    // Simplify only if there are dag terms to simplify otherwise Lambdapi will over simplify everything and the proof will fail.
-    if dag_terms.len() > 0 {
-        proof.push(ProofStep::Simplify(Vec::from_iter(dag_terms)));
-    }
-
+    // Encode: rewrite left  .[x in x = _] reify_correct;
     proof.push(ProofStep::Rewrite(
         true,
         Some("[x in x = _]".into()),
@@ -326,21 +299,13 @@ fn translate_arith_poly_norm(
         vec![],
         SubProofs(None),
     ));
-    proof.push(ProofStep::Rewrite(
-        true,
-        Some("[x in _ = x]".into()),
-        Term::from("reify_correct"),
-        vec![],
-        SubProofs(None),
-    ));
 
-    let (left, right) = match_term!((= l r) = clause[0]).expect("no equality");
-
+    // Encode: set l ≔ (reify e1);
+    let (left, _) = match_term!((= l r) = clause[0]).expect("no equality");
     let e1: Term = Term::Terms(vec!["reify".into(), left.into()]);
-    let e2: Term = Term::Terms(vec!["reify".into(), right.into()]);
     proof.push(ProofStep::Set(l_set_id.to_string(), e1));
-    proof.push(ProofStep::Set(r_set_id.to_string(), e2));
 
+    // Encode: rewrite .[x in val x = _] eta_prod;
     proof.push(ProofStep::Rewrite(
         false,
         Some("[x in val x = _]".into()),
@@ -348,6 +313,17 @@ fn translate_arith_poly_norm(
         vec![],
         SubProofs(None),
     ));
+
+    // Encode: rewrite left .[x in _ = x] (reify_correct_withenv (l ₂));
+    proof.push(ProofStep::Rewrite(
+        true,
+        Some("[x in _ = x]".into()),
+        Term::from("reify_correct_withenv"),
+        vec![Term::from(format!("({} ₂)", l_set_id))],
+        SubProofs(None),
+    ));
+
+    // Encode: rewrite .[x in _ = val x] eta_prod;
     proof.push(ProofStep::Rewrite(
         false,
         Some("[x in _ = val x]".into()),
@@ -356,27 +332,25 @@ fn translate_arith_poly_norm(
         SubProofs(None),
     ));
 
+    // Encode: rewrite left  .[x in x = _] norm_correct;
     proof.push(ProofStep::Rewrite(
         true,
-        None,
+        Some("[x in x = _]".into()),
         Term::from("norm_correct"),
-        vec![
-            Term::Terms(vec![l_set_id.into(), Term::from("₁")]),
-            Term::Terms(vec![l_set_id.into(), Term::from("₂")]),
-        ],
+        vec![],
         SubProofs(None),
     ));
 
+    // Encode: rewrite left  .[x in _ = x] norm_correct;
     proof.push(ProofStep::Rewrite(
         true,
-        None,
+        Some("[x in _ = x]".into()),
         Term::from("norm_correct"),
-        vec![
-            Term::Terms(vec![r_set_id.into(), Term::from("₁")]),
-            Term::Terms(vec![r_set_id.into(), Term::from("₂")]),
-        ],
+        vec![],
         SubProofs(None),
     ));
+
+    // Encode: reflexivity;
     proof.push(ProofStep::Reflexivity);
 
     proof
@@ -613,17 +587,8 @@ fn translate_bool_or_flatten() -> Vec<ProofStep> {
 ///      (=> (or y2 ys) z)
 ///      (and (=> y1 z) _))
 /// ```
-fn translate_bool_implies_or_distrib(
-    args: &[Rc<AletheTerm>],
-    dag_terms: HashSet<String>,
-) -> Vec<ProofStep> {
+fn translate_bool_implies_or_distrib(args: &[Rc<AletheTerm>]) -> Vec<ProofStep> {
     let mut proof = vec![];
-
-    if dag_terms.len() > 0 {
-        dag_terms
-            .into_iter()
-            .for_each(|s| proof.push(ProofStep::Simplify(vec![s.into()])));
-    }
 
     let y1: Term = args[0].clone().into();
     let y2: Term = args[1].clone().into();
