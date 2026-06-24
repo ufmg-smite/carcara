22a0e4bf8213a321f2b7dd4a798f3e8ba5f6153e
Haniel Barbosa <hanielbbarbosa@gmail.com>
2026-06-24 13:03:37 -0300

    Fix `abs` sort

--- carcara/src/ast/pool/mod.rs
+++ carcara/src/ast/pool/mod.rs
@@ -223,7 +223,8 @@ impl PrimitivePool {
                     }
                 }
                 Operator::Ite => self.compute_sort(&args[1]).as_sort().unwrap().clone(),
-                Operator::Add | Operator::Sub | Operator::Mult | Operator::Abs => {
+                Operator::Abs => self.compute_sort(&args[0]).as_sort().unwrap().clone(),
+                Operator::Add | Operator::Sub | Operator::Mult => {
                     if args
                         .iter()
                         .any(|a| self.compute_sort(a).as_sort().unwrap() == &Sort::Real)
