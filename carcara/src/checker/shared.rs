9bc704d4c83ed01584ad3f915fa472d5791df1d6
netolcc06 <netolcc06@gmail.com>
2026-07-15 21:01:47 -0300

    Remove comment

--- carcara/src/checker/shared.rs
+++ carcara/src/checker/shared.rs
@@ -202,7 +202,6 @@ pub fn get_rule_shared(rule_name: &str, elaborated: bool) -> Option<crate::check
         "la_disequality" => linear_arithmetic::la_disequality,
         "la_totality" => linear_arithmetic::la_totality,
         "la_tautology" => linear_arithmetic::la_tautology,
-        // We allow the usage of legacy rules `bv_poly_simp(_eq)`
         "poly_simp" => polynomial::poly_simp,
         "poly_simp_rel" => polynomial::poly_simp_rel,
         "forall_inst" => quantifier::forall_inst,
